361778af1ed60eb36bf3cc9d771f9284920b6ed3
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-11 12:05:29 +0100

    wip

--- src/Components.jsx
+++ src/Components.jsx
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 
 
 ReactDOM.render(
-  <ReturnsObject
-    greet='Hello'
-    person={{ name: 'John' }}
-    desiredClass='some-class'
-  />,
+  <TakesData data={['apple', 'orange']} />,
   document.querySelector('#target7'),
 );
 
@@ -30,7 +26,7 @@ function ReturnsString() {
 }
 
 // Demonstrate a component that returns object
-function ReturnsObject({ person, greet, desiredClass }) {
+function ReturnGreeting({ person, greet, desiredClass }) {
   // return person; // ERROR!!!
   return (
     <div className={desiredClass}>
@@ -42,22 +38,78 @@ function ReturnsObject({ person, greet, desiredClass }) {
 // Demonstrate a component that returns a single child element,
 // both HTML element or another React element
 function ReturnsElement() {
+  return (
+    <div className='something'>
+      <div>
+        <ReturnGreeting
+          person={{ name: 'Tom' }}
+          greet='Howdy'
+          desiredClass='kind-greet'
+        />
+      </div>
 
+      <ReturnGreeting
+        person={{ name: 'Luke' }}
+        greet='What is up'
+        desiredClass='super-kind-greet'
+      />
+
+      <footer>
+        <ReturnGreeting
+          person={{ name: 'Samar' }}
+          greet='Hello'
+          desiredClass='really-kind-greet'
+        />
+      </footer>
+    </div>
+  );
 }
 
 // Demonstrate a component that returns an array
 function ReturnsArray() {
-
+  return [
+    <ReturnGreeting
+      person={{ name: 'Samar' }}
+      greet='Hello'
+      desiredClass='really-kind-greet'
+    />,
+    <ReturnGreeting
+      person={{ name: 'Luke' }}
+      greet='Hello'
+      desiredClass='really-kind-greet'
+    />,
+    'Hello',
+  ];
 }
 
 // Demonstrate a component that returns multiple children
 function ReturnsMultipleChildren() {
-
+  return (
+    <>
+      <ReturnGreeting
+        person={{ name: 'Samar' }}
+        greet='Hello'
+        desiredClass='really-kind-greet'
+      />
+      <ReturnGreeting
+        person={{ name: 'Luke' }}
+        greet='Hello'
+        desiredClass='really-kind-greet'
+      />
+      Wohooooo
+    </>
+  );
 }
 
 // Demonstrate injecting a string through props and interpolating it
 function TakesData({ data }) {
-
+  return (
+    <>
+      {
+        data.map(fruit => <span>{fruit}</span>)
+      }
+    </>
+  );
 }
 
 // Demonstrate injecting a number through props and using it
@@ -73,5 +125,5 @@ function TakesArray({ data }) {
 // Build a component that takes a number, and returns an h1
 // saying whether you passed an odd or even number.
 function EvenOrOdd({ data }) {
-
+  
 }
